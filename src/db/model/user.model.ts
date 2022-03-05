14859f28039b332670e87fd15b9c8e8e68ad36ec
Motone Adachi <waricoma@devne.co>
2022-03-05 11:39:15 +0900

    fix: user session system

--- src/db/model/user.model.ts
+++ src/db/model/user.model.ts
@@ -4,7 +4,7 @@ import { User } from '@prisma/client';
 import { Model } from './_model';
 import { hash, hashCompare } from '../../utils/hash'
 import { SESSION_SECRET } from '../../constants/constant';
-import {v5 as uuidv5} from 'uuid'
+import { v4 as uuidV4,  v5 as uuidV5 } from 'uuid'
 
 export class UserModel extends Model {
   public async getBySession(session: string): Promise<User> {
@@ -15,8 +15,8 @@ export class UserModel extends Model {
     });
   }
 
-  public async enableSession(name:string): Promise<string> {
-    const session = uuidv5(name,'a3c134a8-c24b-47f3-ad2d-2aa34b197efc');
+  public async enableSession(name: string): Promise<string> {
+    const session = uuidV5(name + SESSION_SECRET, uuidV4());
     this.client.user.update({
       where: { name },
       data: { session }
@@ -24,17 +24,13 @@ export class UserModel extends Model {
     return session;
   }
 
-  //public async allKill() {
-  //  await this.client.user.deleteMany()
-  //}
-
   public async add(name: string, pass:string): Promise<User> {
     const hashStr = hash(pass);
     return await this.client.user.create({
       data: {name, hash: hashStr}
     });
   };
-  
+
   public async userExist(name: string, pass: string): Promise<User> {
     const hitUser = await this.client.user.findUnique({
       where: {
