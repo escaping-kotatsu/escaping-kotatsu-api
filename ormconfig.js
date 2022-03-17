f95e8fb70319e604308a7b57318dd86f061ebeea
Motone Adachi <waricoma@devne.co>
2022-03-17 23:28:48 +0900

    fix: orm conf

--- ormconfig.js
+++ ormconfig.js
@@ -5,10 +5,8 @@ const { REGISTER_INSTANCE } = require('ts-node');
 module.exports = {
   type: 'postgres',
   synchronize: true,
-  entities: [
-    process[REGISTER_INSTANCE] ? './src/db/entities/*.entity.ts' : `${__dirname}/dist/src/db/entities/*.entity.js`,
-  ],
-  migrations: [process[REGISTER_INSTANCE] ? './src/db/migrations/*.ts' : `${__dirname}/dist/src/db/migrations/*.js`],
-  subscribers: [process[REGISTER_INSTANCE] ? './src/db/subscribers/*.ts' : `${__dirname}/dist/src/db/subscribers/*.js`],
+  entities: [process[REGISTER_INSTANCE] ? './src/db/entities/*.entity.ts' : './dist/db/entities/*.entity.js'],
+  migrations: [process[REGISTER_INSTANCE] ? './src/db/migrations/*.ts' : './dist/db/migrations/*.js'],
+  subscribers: [process[REGISTER_INSTANCE] ? './src/db/subscribers/*.ts' : './dist/db/subscribers/*.js'],
   url: process.env.DATABASE_URL,
 };
